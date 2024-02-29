993c216feb69b78dba4f51aab6e0649cab6eb746
Jaejin Lee <100942304+ArchMelow@users.noreply.github.com>
2024-02-29 19:36:33 +0900

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -75,6 +75,7 @@ function build_esp32 {
     idf.py add-dependency "espressif/mdns^1.2.4"
     idf.py add-dependency "esp_tinyusb^1.0.0"
     rm -rf builds
+    echo $(pwd)
     idf.py -B ../../../dependencies/micropython/ports/esp32/build-${BOARD} build
 
     cd ../../../
