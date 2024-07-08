853609e169165d672528f00ba0e09276efb82137
Jaejin Lee <100942304+ArchMelow@users.noreply.github.com>
2024-07-09 00:15:06 +0900

    Update micropython.cmake

--- micropython-modules/micropython.cmake
+++ micropython-modules/micropython.cmake
@@ -43,8 +43,11 @@ endif()
 
 include(${CMAKE_CURRENT_LIST_DIR}/../dependencies/micropython-ulab/code/micropython.cmake)
 
+include(${CMAKE_CURRENT_LIST_DIR}/../dependencies/micropython-camera-driver/src/micropython.cmake)
+
+
 # the camera driver
-#include(${CMAKE_CURRENT_LIST_DIR}/../dependencies/micropython-camera-driver/src/micropython.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/../dependencies/micropython-camera-driver/src/micropython.cmake)
 
 
 
