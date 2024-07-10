7c2700b636bcf16f7079c53c7d52640d1a005571
bhlee420 <BHLEE420@GMAIL.COM>
2024-07-10 20:46:24 +0900

    Update modcamera.h

--- micropython-modules/micropython-camera-driver/modcamera.h
+++ micropython-modules/micropython-camera-driver/modcamera.h
@@ -24,21 +24,21 @@ enum { OV2640, OV7725};
 //WROVER-KIT PIN Map
 #define CAM_PIN_PWDN    32 //power down is not used
 #define CAM_PIN_RESET   -1 //software reset will be performed
-#define CAM_PIN_XCLK     0
-#define CAM_PIN_SIOD    26 // SDA
-#define CAM_PIN_SIOC    27 // SCL
+#define CAM_PIN_XCLK     10  //0
+#define CAM_PIN_SIOD    40 // 26 : SDA
+#define CAM_PIN_SIOC    39 // 27 : SCL
 
-#define CAM_PIN_D7      35
-#define CAM_PIN_D6      34
-#define CAM_PIN_D5      39
-#define CAM_PIN_D4      36
-#define CAM_PIN_D3      21
-#define CAM_PIN_D2      19
-#define CAM_PIN_D1      18
-#define CAM_PIN_D0       5
-#define CAM_PIN_VSYNC   25
-#define CAM_PIN_HREF    23
-#define CAM_PIN_PCLK    22
+#define CAM_PIN_D7      48 // 35
+#define CAM_PIN_D6      11 // 34
+#define CAM_PIN_D5      12 // 39
+#define CAM_PIN_D4      14 // 36
+#define CAM_PIN_D3      16 // 21
+#define CAM_PIN_D2      18 // 19
+#define CAM_PIN_D1      17 // 18
+#define CAM_PIN_D0      15 // 5
+#define CAM_PIN_VSYNC    38 // 25
+#define CAM_PIN_HREF    47 // 23
+#define CAM_PIN_PCLK    13 // 22
 #define XCLK_FREQ_10MHz    10000000
 #define XCLK_FREQ_20MHz    20000000
 
