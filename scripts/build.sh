88c55bc84eb4d9928f3096317d503fe82bb1dcf5
bhlee420 <BHLEE420@GMAIL.COM>
2024-07-19 17:51:47 +0900

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -75,7 +75,7 @@ function build_esp32 {
     echo "LEE PROMPT : BUILDING ${BOARD}.."
     idf.py add-dependency "espressif/mdns^1.2.4"
     idf.py add-dependency "esp_tinyusb^1.0.0"
-    idf.py add-dependency "espressif/esp32-camera"
+    #idf.py add-dependency "espressif/esp32-camera"
     rm -rf builds
     PWd=$(pwd)
     echo "Lee dir :${PWd} !"
