c761b425da8c519c203d1eee9569d600cf8d1d31
bhlee420 <BHLEE420@GMAIL.COM>
2024-07-19 16:17:51 +0900

    Update mpconfigboard.h

--- boards/esp32/MICROLITE_SPIRAM_CAM/mpconfigboard.h
+++ boards/esp32/MICROLITE_SPIRAM_CAM/mpconfigboard.h
@@ -1,11 +1,12 @@
 //#define MICROPY_HW_BOARD_NAME "ESP32-cam module (microlite)"
 #define MICROPY_HW_BOARD_NAME "ESP32S3-cam module (microlite-spiram)"
 #define MICROPY_HW_MCU_NAME "ESP32S3"
- 
+
+
 #define MICROPY_PY_BLUETOOTH                (0)
 #define MICROPY_PY_MACHINE_DAC              (0)
-#define MODULE_CAMERA_ENABLED               (1)
-
+//#define MODULE_CAMERA_ENABLED               (1)
+#define MICROPYTHON-CAMERA-DRIVER_ENABLED               (1)
 
 #define MICROPY_HW_I2C0_SCL                 (9)
 #define MICROPY_HW_I2C0_SDA                 (8)
